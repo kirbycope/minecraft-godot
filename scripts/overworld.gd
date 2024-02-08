deca5106a3c828ae2535613175bb3158820348e5
Timothy Cope <kirbycope@gmail.com>
2024-02-08 14:57:47 -0800

    pickup tres

--- scripts/overworld.gd
+++ scripts/overworld.gd
@@ -3,22 +3,41 @@ extends Node2D
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
-		var source_id = get_cell_source_id()
+		# Get cell at "Base Layer" of TileMap
+		var source_id = get_cell_source_id(1)
+		
 		# Use "wood_hoe" to make "farmland_dry"
 		if Global.selected_item == "wood_hoe":
-			if (source_id == 2 # Source ID 2: "grass"
-			or source_id == 8): # Source ID 8: "grass_path"
-				set_cell(1, "farmland_dry")
+			if (source_id == 2 					# Source ID 2: "grass"
+			or source_id == 8): 				# Source ID 8: "grass_path"
+				set_cell(1, 21) 				# Source ID 21: "farmland_dry"
+		
 		# Use "wood_shovel" to make "grass_path"
 		if Global.selected_item == "wood_shovel":
-			if(source_id == 2 # Source ID 2: "grass"
-			or source_id == 21): # Source ID 21: "farmland_dry"
-				set_cell(1, "grass_path")
+			if(source_id == 2 					# Source ID 2: "grass"
+			or source_id == 21): 				# Source ID 21: "farmland_dry"
+				set_cell(1, 8) 					# Source ID 8: "grass_path"
+		
 		# Use "seeds_wheat" on "farmland_(dry/wet)" to plant "wheat"
 		if Global.selected_item == "seeds_wheat":
-			if(source_id == 20 # Source ID 20: "farmland_wet"
-			or source_id == 21): # Source ID 21: "farmland_dry"
-				set_cell(2,"wheat_1")
+			if(source_id == 20 					# Source ID 20: "farmland_wet"
+			or source_id == 21): 				# Source ID 21: "farmland_dry"
+				set_cell(2, 22) 				# Source ID 22: "wheat"
+		
+		# Use "sapling_oak" on grass/path/farmland to plant "sapling_oak"
+		if Global.selected_item == "sapling_oak":
+			if(source_id == 2 					# Source ID 2: "grass",
+			or source_id == 8 					# Source ID 8: "grass_path"
+			or source_id == 20 					# Source ID 20: "farmland_wet"
+			or source_id == 21): 				# Source ID 21: "farmland_dry"
+				set_cell(2, 9) 					# Source ID 9: "sappling_oak"
+		
+	if event.is_action_pressed("ui_select"):
+		# Get cell at "Breakable Layer" of TileMap
+		var source_id = get_cell_source_id(2)
+		if source_id == 9: # Source ID 9: "sappling_oak"
+			$TileMap.erase_cell(2, get_map_position())
+			give_item("sapling_oak")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -31,13 +50,18 @@ func _process(delta):
 	pass
 
 
-# Get the ID of the cell the player is standing on (see TileMap > TileSet > Tiles).
-func get_cell_source_id():
+# Gets the player's current map position
+func get_map_position():
 	var player = $TileMap/player
 	var tilemap = $TileMap
 	var player_position = player.position
 	var map_position = tilemap.local_to_map(player_position)
-	return tilemap.get_cell_source_id(1, map_position)
+	return map_position
+
+
+# Get the ID of the cell the player is standing on (see TileMap > TileSet > Tiles).
+func get_cell_source_id(layer):
+	return $TileMap.get_cell_source_id(layer, get_map_position())
 
 
 # Get the friendly name of the given tile Source ID.
@@ -92,18 +116,23 @@ func get_tile_name(source_id):
 		return "water"
 
 
+func give_item(item_name):
+	var item = {}
+	item.id = item_name
+	item.texture = "res://textures/" + item_name + ".png"
+	Global.add_item_to_inventory(item)
+	# Update the UI
+	var hud_node = $TileMap/player/hud
+	hud_node.show_actionbar_items()
+	hud_node.show_crafting_table_items()
+	hud_node.show_single_chest_items()
+
+
 # Set the tile at the player's position.
-func set_cell(layer, tile_name):
-	var player = $TileMap/player
-	var tilemap = $TileMap
-	var player_position = player.position
-	var map_position = tilemap.local_to_map(player_position)
-	if tile_name == "farmland_dry": # Source ID: 21
-		tilemap.set_cell(layer, map_position, 21, Vector2i(0, 0), 0)
+func set_cell(layer, source_id):
+	var map_position = get_map_position()
+	$TileMap.set_cell(layer, map_position, source_id, Vector2i(0, 0), 0)
+	if source_id == 21:
 		$TileMap/player/dig_grass.play()
-	elif tile_name == "grass_path": # Source ID: 8
-		tilemap.set_cell(layer, map_position, 8, Vector2i(0, 0), 0)
+	elif source_id == 8:
 		$TileMap/player/step_grass.play()
-	elif tile_name == "wheat_1": # Source ID: 22
-		tilemap.set_cell(layer, map_position, 22, Vector2i(0, 0), 0)
-		#$TileMap/player/dig_grass.play()
