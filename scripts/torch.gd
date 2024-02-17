f68b59dcd11d13198bfd1927982f1a89382d0ae8
Timothy Cope <kirbycope@gmail.com>
2024-02-17 12:53:54 -0800

    attack to pick up torch

--- scripts/torch.gd
+++ scripts/torch.gd
@@ -3,6 +3,9 @@
 extends Area2D
 
 
+var player_in_range = false
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,14 +16,24 @@ func _process(delta):
 	# Hide if not day
 	$PointLight2D.visible = not Global.day
 
+func _input(event):
+	if player_in_range:
+		if event.is_action_pressed("Attack"):
+			# Give player "torch"
+			var item = {}
+			item.id = "torch"
+			item.stack_size = 1
+			item.quantity = 1
+			item.texture = "res://textures/torch.png"
+			Global.add_item_to_inventory(item)
+			queue_free()
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		# Give player "torch"
-		var item = {}
-		item.id = "torch"
-		item.stack_size = 1
-		item.quantity = 1
-		item.texture = "res://textures/torch.png"
-		Global.add_item_to_inventory(item)
-		queue_free()
+		player_in_range = true
+
+
+func _on_body_exited(body):
+	if body.is_in_group("Player"):
+		player_in_range = false
+
